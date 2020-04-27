21c3492d8b70e2804a55bbbc9166e98c1ec624e1
Crowdbotics <team@crowdbotics.com>
2020-04-27 06:31:13 +0000

    Update models

--- backend/free_robux_16292/wsgi.py
+++ backend/free_robux_16292/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'free_robux_16292.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "free_robux_16292.settings")
 
 application = get_wsgi_application()
